400211a76e3aaf709bc687ff9c4f28d4f1a1e5ca
sebastien <ndase15ba@gmail.com>
2020-11-14 17:14:39 +0200

    add crud for authentication

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\authController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// authentication apis
+// Route::get('logout', [authController::class, 'logout']);
+// Route::get('users', [authController::class, 'getUsers']);
+// Route::post('register', [authController::class, 'register']);
+// Route::get('users/{id}', [authController::class, 'getUser']);
+// Route::put('users/{id}', [authController::class, 'updateUser']);
+// Route::delete('users/{id}', [authController::class, 'deleteUser']);
+// Route::post('login', [authController::class, 'authenticate']);
